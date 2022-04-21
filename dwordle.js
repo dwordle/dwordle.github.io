639f6a88f0174cdfa25576b0ed4cb590372d477e
dwordle <juliuszkarolak+dwordle@gmail.com>
2022-04-21 15:38:11 +0200

    you better ducking work now

--- dwordle.js
+++ dwordle.js
@@ -11,16 +11,24 @@ const randomDword = () => {
 
 const hasMaxChars = (num, max) => num < 10 ** max;
 
-const dwordToCharArray = (dword) => {
+const dwordToCharArray = dword => {
 	num = Math.abs(dword);
 	let s = [];
 	for (let i = 0; i < MAX_CHARS; ++i) {
 		if (num >= 10 ** (MAX_CHARS - i - 1))
-			s[i] = Math.floor(num / 10 ** (MAX_CHARS - i - 1)) % 10;
+			s[i] = `${Math.floor(num / 10 ** (MAX_CHARS - i - 1)) % 10}`;
 		else
 			s[i] = (i == 0 && dword < 0) ? "-" : "0";
 	}
 	return s;
+}
+
+const stringToCharArray = string => {
+	let arr = [];
+	for (let i = 0; i < MAX_CHARS; ++i) {
+		arr[i] = string[i];
+	}
+	return arr;
 }
 
 const getMatchArray = (generatedCharArray, userCharArray) => {
@@ -40,3 +48,61 @@ const getMatchArray = (generatedCharArray, userCharArray) => {
 	}
 	return matches;
 }
+
+const game = {};
+
+const validateField = () => {
+	game.btn.disabled = game.guess.value.length != MAX_CHARS;
+}
+
+const refresh = () => {
+	game.left.innerText = game.attempts;
+	game.boardEl.innerText = "";
+	for (let row of game.board) {
+		let rowEl = document.createElement("div");
+		rowEl.className = "board-row";
+		for (let i in row.chars) {
+			let char = document.createElement("div");
+			char.className = `board-char ${["not-in-row", "in-row", "exact"][row.matches[i]]}`;
+			char.innerText = row.chars[i];
+			rowEl.appendChild(char);
+		}
+		game.boardEl.appendChild(rowEl);
+	}
+}
+
+const reset = () => {
+	let dword = randomDword();
+	console.log(dword);
+	game.dwordArray = dwordToCharArray(dword);
+	game.attempts = 6;
+	game.board = [];
+	game.guess.value = "";
+	validateField();
+	refresh();
+}
+
+const initDwordle = (guessId, boardId, leftId, btnId) => {
+	game.guess = document.getElementById(guessId);
+	game.boardEl = document.getElementById(boardId);
+	game.left = document.getElementById(leftId);
+	game.btn = document.getElementById(btnId);
+	validateField();
+	reset();
+}
+
+const check = () => {
+	if (!game.guess)
+		throw "Dwordle not initialized";
+	let userCharArray = stringToCharArray(game.guess.value);
+	game.board.push({
+		chars: userCharArray,
+		matches: getMatchArray(game.dwordArray, userCharArray)
+	});
+	--game.attempts;
+	game.guess.value = "";
+	validateField();
+	refresh();
+}
+
+//
